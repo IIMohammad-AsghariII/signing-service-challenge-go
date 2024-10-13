c9353d917e89d5bd59623bd2f94e898f0257d115
Mohammad <tiimohammad.asghariiit@gmail.com>
2024-10-13 13:58:23 +0200

    Update main go

--- main.go
+++ main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"log"
+	"os"
+	"regexp"
 
 	"github.com/fiskaly/coding-challenges/signing-service-challenge/api"
 )
 
-const (
-	ListenAddress = ":8080"
-	// TODO: add further configuration parameters here ...
-)
-
 func main() {
-	server := api.NewServer(ListenAddress)
+
+	// Regex to match the current working directory
+	re := regexp.MustCompile(`^(.*` + "signing-service-challenge-go" + `)`)
+	cwd, _ := os.Getwd()
+	rootPath := re.Find([]byte(cwd))
+
+	// Load environment variables from .env file
+	if err := godotenv.Load(string(rootPath) + `/.env`); err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	// Get the DATA_STORE environment variable
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // default port
+	}
+	listenAddress := ":" + port
+	server := api.NewServer(listenAddress)
 
 	if err := server.Run(); err != nil {
-		log.Fatal("Could not start server on ", ListenAddress)
+		log.Fatal("Could not start server on ", listenAddress)
 	}
 }
